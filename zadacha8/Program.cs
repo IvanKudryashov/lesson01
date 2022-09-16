5e23fcb0840228cf1d43a37c145eb6d2c3d914be
legolav <bbabkova4@gmail.com>
2022-09-16 19:04:21 +0300

    добавлено 4 задачи

---
+++ zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿//программа, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (N >= a)
+{
+    if ((a%2) == 0)
+    {
+        Console.Write(a + " ");
+    }
+    a++;
+}
